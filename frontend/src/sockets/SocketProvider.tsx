a9f489047b506eb0ff5a4e82c727980f2ce0559e
Eyob Teshome <yob.teshome27@gmail.com>
2025-10-10 14:32:53 +0300

    dockerize (#7)

--- frontend/src/sockets/SocketProvider.tsx
+++ frontend/src/sockets/SocketProvider.tsx
@@ -1,10 +1,4 @@
-import React, {
-  createContext,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { useAuth } from '../contexts/AuthContext';
 
